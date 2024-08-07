778a362f819f3b5dad8e265dc01d264456a76bb5
Hartmut Obendorf <hartmut@obendorf.de>
2024-08-07 15:42:55 +0200

    progress bar

--- examples/live_progress.py
+++ examples/live_progress.py
@@ -8,14 +8,15 @@ from time import sleep
 
 from rich.live import Live
 from rich.panel import Panel
-from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
+from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn, TimeElapsedColumn, TimeRemainingColumn
+from rich.progress_bar import PredefinedStyles
 from rich.table import Table
 
 
 job_progress = Progress(
     "{task.description}",
     SpinnerColumn(),
-    BarColumn(),
+    BarColumn(bar_style=PredefinedStyles.BLOCK),
     TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
 )
 job1 = job_progress.add_task("[green]Cooking")
@@ -23,7 +24,14 @@ job2 = job_progress.add_task("[magenta]Baking", total=200)
 job3 = job_progress.add_task("[cyan]Mixing", total=400)
 
 total = sum(task.total for task in job_progress.tasks)
-overall_progress = Progress()
+overall_progress = Progress(
+    "{task.description}",
+    SpinnerColumn(),
+    BarColumn(bar_style=PredefinedStyles.BLOCK_FINE),
+    TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),    
+    TimeElapsedColumn(),
+    TimeRemainingColumn(),
+)
 overall_task = overall_progress.add_task("All Jobs", total=int(total))
 
 progress_table = Table.grid()
